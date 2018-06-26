cbf7bc762f9f61f2581a3062621626fd2321798a
rsmoreira <rsmoreira.mail@gmail.com>
2018-06-26 07:02:39 -0300

    Added redux-form and started working on the first SurveyNew components and on the generic Field Components.

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+// Survey field contains logic to render single label and 
+// text input
+import React from 'react';
+
+export default({ input }) => {
+    return (
+        <div>
+            <input {...input} />
+        </div>
+    );
+};
