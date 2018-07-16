c9ff3d7dd439f3c1f8d6691c70c2fa6bbdcbf25d
rsmoreira <rsmoreira.mail@gmail.com>
2018-07-15 21:38:06 -0300

    Added webhook using LocalTunnel to have a communication between our survey link sent to the e-mail and our rest services. It was also added code to persist the survey answers.

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -9,10 +9,10 @@ module.exports = survey => {
                     <p>Please enter the following question:</p>
                     <p>${survey.body}</p>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/thanks">YES</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">YES</a>
                     </div>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/thanks">NO</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">NO</a>
                     </div>
                 </div>
             </body>
